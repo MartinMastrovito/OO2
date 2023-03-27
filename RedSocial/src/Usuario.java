38eb66772679eb942612c8539ab5bff60cc69148
Martin Mastrovito <80993120+MartinMastrovito@users.noreply.github.com>
2023-03-27 18:30:47 -0300

    Update Usuario.java

--- RedSocial/src/Usuario.java
+++ RedSocial/src/Usuario.java
@@ -1,4 +1,32 @@
 
-public class Usuario {
+	package ar.edu.info.unlp.ejercicioDemo;
 
-}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+	private List <Tweet> Tweets;
+	private String nom;
+	
+	
+	public Usuario( String nom) {
+		super();
+		Tweets = new ArrayList<Tweet>();
+		this.nom = nom;
+	}
+
+	public void elimMen () {
+		this.Tweets.clear();
+	}
+
+	public List<Tweet> getTweets() {
+		return Tweets;
+	}
+
+	public void setTweets(ArrayList<Tweet> tweets) {
+		Tweets = tweets;
+	}
+
+	public String getNom() {
+		return nom;
+	
