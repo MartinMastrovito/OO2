7ca84bd5b83a6b390c8a2d96e80f0ce029afa1b4
Martin Mastrovito <80993120+MartinMastrovito@users.noreply.github.com>
2023-03-27 18:29:42 -0300

    Update Tweet.java

--- RedSocial/src/Tweet.java
+++ RedSocial/src/Tweet.java
@@ -1,4 +1,21 @@
+package ar.edu.info.unlp.ejercicioDemo;
+
+import java.util.ArrayList;
 
 public class Tweet {
-
-}
+	private Tweet Origen;
+	private boolean esReTweet;
+	private String Text;
+	private Usuario owner;
+	
+	public Tweet (Usuario owner, String text) {
+		if (text.length() > 0 || text.length() < 281) {
+			this.Text=text;
+			this.owner=owner;
+		}
+		else {
+			throw new IllegalArgumentException(" cantidad de caracteres incorrectos");
+		}
+	}
+	
+	
